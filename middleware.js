633ce807fdc7f354d1c66b5e9b985006398ea7b1
writer169 <148839750+writer169@users.noreply.github.com>
2025-05-18 16:54:37 +0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,12 +1,19 @@
-// middleware.js
+// middleware.ts
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    auth().protect(); // Защищаем маршруты, соответствующие /dashboard
+  }
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Применять middleware ко всем маршрутам, кроме статических файлов и _next
+    '/((?!.*\\..*|_next).*)',
+    '/',
+    '/(api|trpc)(.*)',
+  ],
 };
